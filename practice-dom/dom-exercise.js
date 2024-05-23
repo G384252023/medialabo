ae9e568351d6db81c9aa55147aa68d72f51d1009
G384252023 <238425@st.takushoku-u.ac.jp>
2024-05-23 17:50:28 +0900

    DOM

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -1,45 +1,54 @@
 //// 注意: 以下は編集しないこと!
 let data = [
-	{name:'札幌', lat:43.06417, lng:141.34694},
-	{name:'仙台', lat:38.26889, lng:140.87194},
-	{name:'新宿', lat:35.68944, lng:139.69167},
-	{name:'名古屋', lat:35.18028, lng:136.90667},
-	{name:'大阪', lat:34.68639, lng:135.52},
-	{name:'広島', lat:34.39639, lng:132.45944},
-	{name:'高知', lat:33.55972, lng:133.53111},
-	{name:'福岡', lat:33.60639, lng:130.41806},
-	{name:'鹿児島', lat:31.56028, lng:130.55806},
-	{name:'沖縄', lat:26.2125, lng:127.68111}
+	{ name: '札幌', lat: 43.06417, lng: 141.34694 },
+	{ name: '仙台', lat: 38.26889, lng: 140.87194 },
+	{ name: '新宿', lat: 35.68944, lng: 139.69167 },
+	{ name: '名古屋', lat: 35.18028, lng: 136.90667 },
+	{ name: '大阪', lat: 34.68639, lng: 135.52 },
+	{ name: '広島', lat: 34.39639, lng: 132.45944 },
+	{ name: '高知', lat: 33.55972, lng: 133.53111 },
+	{ name: '福岡', lat: 33.60639, lng: 130.41806 },
+	{ name: '鹿児島', lat: 31.56028, lng: 130.55806 },
+	{ name: '沖縄', lat: 26.2125, lng: 127.68111 }
 ];
 //// 注意: 以上は編集しないこと!
 
 // 練習4-2 メッセージ追加プログラム
 let p1 = document.querySelector('h2#ex42');
 console.log(p1);
-let pc=document.createElement('pc');
-pc.textContent='写真表と都市の緯度軽度のページです';
-p1.insertAdjacentElement('afterend',pc)
-pc=document.querySelector('pc');
-pc.style.textEmphasis='sesame green';
+let pc = document.createElement('pc');
+pc.textContent = '写真表と都市の緯度軽度のページです';
+p1.insertAdjacentElement('afterend', pc)
+pc = document.querySelector('pc');
+pc.style.textEmphasis = 'sesame green';
 
 // 練習4-3 写真表作成プログラム
-let img_1=document.createElement('img');
-let img_2=document.createElement('img');
-let img_3=document.createElement('img');
-img_1.setAttribute('src',"taro.png"); 
-img_2.setAttribute('src',"jiro.png"); 
-img_3.setAttribute('src',"hanako.png"); 
-let p2=document.querySelector('div#phototable'); 
-p2.insertAdjacentElement('beforeend',img_1); 
-p2.insertAdjacentElement('beforeend',img_2); 
-p2.insertAdjacentElement('beforeend',img_3); 
+let img_1 = document.createElement('img');
+let img_2 = document.createElement('img');
+let img_3 = document.createElement('img');
+img_1.setAttribute('src', "taro.png");
+img_2.setAttribute('src', "jiro.png");
+img_3.setAttribute('src', "hanako.png");
+let p2 = document.querySelector('div#phototable');
+p2.insertAdjacentElement('beforeend', img_1);
+p2.insertAdjacentElement('beforeend', img_2);
+p2.insertAdjacentElement('beforeend', img_3);
 
-img_1.setAttribute('width','200'); 
-img_2.setAttribute('width','200'); 
-img_3.setAttribute('width','200'); 
+img_1.setAttribute('width', '200');
+img_2.setAttribute('width', '200');
+img_3.setAttribute('width', '200');
 
-// 練習4-4 箇条書き削除プログラム
-let w=document.querySelector('ul#location');
-w.remove();
+// 練習4-4 箇条ƒ書き削除プログラム
+let w = document.querySelector('ul#location');
+let w1 = document.querySelectorAll('li');
+for (let li of w1) {
+	li.remove();
+}
 // 練習4-5 箇条書き追加プログラム
 
+let ul5=document.querySelector('ul#location');
+for(let data5 of data){
+ let li =document.createElement('li');
+	li.textContent=data5.name+'...緯度:'+data5.lat+', 経度:'+data5.lng;
+	ul5.insertAdjacentElement('beforeend',li);
+}
